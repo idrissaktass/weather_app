3db9d906902c3af38a00fa79ceb4615347f990b5
idrissaktass <80005089+idrissaktass@users.noreply.github.com>
2025-07-07 14:17:14 +0300

    Update LoadingSpinner.js

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,12 +1,12 @@
-import { CircularProgress, Grid2, Typography } from "@mui/material"
+import { CircularProgress, Grid, Typography } from "@mui/material"
 
 const LoadingSpinner = () => {
 
     return(
-        <Grid2 display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"} minHeight={"100%"} gap={"20px"}>
+        <Grid display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"} minHeight={"100%"} gap={"20px"}>
             <CircularProgress size={70}/>
             <Typography fontSize={"24px"} fontWeight={600}>Loading...</Typography>
-        </Grid2>
+        </Grid>
     )
 }
 
